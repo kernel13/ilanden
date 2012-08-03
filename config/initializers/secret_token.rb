1b571bf3b7f88340b9254809eb13f8ac97f4f883
busyboard <contact@bb.c-coool.com>
2012-08-03 12:08:14 +0200

    Created application using Refinery CMS 2.0.5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ilanden::Application.config.secret_token = '7415cda35fceee98638c75637338d8a86642975e5d34d22447e16ec4412699f2f9fd939e0529a97f122f9aa79bd55814aa355e1cc152273a420bf8e8b0371eae'
